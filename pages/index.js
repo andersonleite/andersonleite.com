19c5a356782100fbbb8b22366a757bfd95200e1e
Anderson Leite <andersonlfl@gmail.com>
2021-08-29 04:45:52 -0700

    update

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,21 @@
-import Link from 'next/link'
-
 export default function IndexPage() {
   return (
     <div className="main">
-      <Link href="/birds">
-        <a>Birds Example</a>
-      </Link>
-      <Link href="/boxes">
-        <a>Boxes Example</a>
-      </Link>
+      <div className="info-container">
+        <div className="me">
+          <h1>Anderson Leite</h1>
+          <div className="circle01"></div>
+          <div className="circle01 c2"></div>
+          <div className="circle01 c3"></div>
+        </div>
+        <div className="description">
+          <p>
+            Currently working as a Technology Lead for <a target="_blank" href="https://www.rga.com/offices/tokyo">R/GA Tokyo</a>.<br/>
+            Previous to that, I worked at <a target="_blank" href="https://www.apple.com/">Apple</a>, <a target="_blank" href="https://www.rga.com/offices/san-francisco">R/GA San Francisco</a> and <a target="_blank" href="https://www.rga.com/offices/sao-paulo">R/GA São Paulo</a>.<br/>
+            Thanks for stopping by, please <a target="_blank" href="https://www.linkedin.com/in/leiteanderson/">get in touch</a>.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
