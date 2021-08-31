e8d9e7e48891b7cf0bfdaaa1a44bc4a71e35975b
Anderson Leite <andersonlfl@gmail.com>
2021-08-30 21:44:51 -0700

    update

--- pages/lab.js
+++ pages/lab.js
@@ -5,7 +5,6 @@ import Box from '../components/Box'
 export default function BoxesPage() {
   return (
     <>
-      <h1>Click on me - Hover me :)</h1>
       <Canvas camera={{ position: [0, 0, 35] }}>
         <ambientLight intensity={2} />
         <pointLight position={[40, 40, 40]} />
