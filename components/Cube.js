5505029a6cfc748adcd096f3f25272c65e84da4e
Anderson Leite <andersonlfl@gmail.com>
2021-09-01 00:23:20 -0700

    add lava gui controls

--- components/Cube.js
+++ components/Cube.js
@@ -1,16 +1,16 @@
-import { useRef, useState } from 'react'
-import { useFrame } from '@react-three/fiber'
+import { useControls } from "leva"
 
-export default function Cube(props) {
-  // const mesh = useRef()
-  // const [hovered, setHover] = useState(false)
-  // const [active, setActive] = useState(false)
-  // useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
+export default function Cube() {
+
+  const { y } = useControls('Cube', { y: {value: 1, min: 0, max: 10, step: 1 }})
+  const { x } = useControls('Cube', { x: {value: 1, min: 0, max: 10, step: 1 }})
+  const { z } = useControls('Cube', { z: {value: 1, min: 0, max: 10, step: 1 }})
+  const { wireframe } = useControls('Cube', { wireframe: true })
 
   return (
     <mesh>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshBasicMaterial color="cyan" wireframe={true} />
+      <boxGeometry args={[x, y, z]} />
+      <meshBasicMaterial color="cyan" wireframe={wireframe} />
     </mesh>
   )
 }
--- components/Scene.js
+++ components/Scene.js
@@ -1,13 +1,21 @@
 import Cube from "./Cube";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from '@react-three/drei'
+import { useControls } from "leva"
 
-export default () =>
-  <Canvas
-    pixelRatio={window.devicePixelRatio}
-    camera={{ position: [0, 0, 3] }}>
+export default function Scene() {
+
+  const { visible } = useControls('Axes Helper', { visible: true })
+  const { size } = useControls('Axes Helper', { size: { value: 1, min: 0, max: 10, step: 1 } })
+
+  return (
+    <Canvas
+      pixelRatio={window.devicePixelRatio}
+      camera={{ position: [0, 0, 3] }
+      }>
       <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
-      <Cube/>
-    <axesHelper />
-  </Canvas>
-
+      <Cube />
+      <axesHelper args={[size]} visible={visible} />
+    </Canvas >
+  )
+}
