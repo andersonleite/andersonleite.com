a4df8dc359b4329d2348cae9cd34ab97c8d35eda
Anderson Leite <andersonlfl@gmail.com>
2021-09-02 14:32:20 -0700

    Load mesh texture and add lights.

--- components/haunted-house/haunted.js
+++ components/haunted-house/haunted.js
@@ -1,12 +1,34 @@
-import { useControls } from "leva"
-import React from 'react'
+import { useControls } from 'leva'
+import { useTexture } from '@react-three/drei'
+import React, { Suspense } from 'react'
+import { Fog } from 'three'
 
-export default function Cube() {
+export default function Haunted() {
   return (
     <>
-      <ambientLight />
-      <Floor />
-      <Walls />
+      <ambientLight args={['#b9d5ff', 0.12]} />
+      <directionalLight args={['#b9d5ff', 0.12]} castShadow={true}
+        shadow-mapSize-width={256}
+        shadow-mapSize-height={256}
+        shadow-camera-far={15}
+        position={[4, 5, - 2]}
+      />
+      <pointLight
+        args={['#fff', 2, 3]}
+        castShadow
+        shadow-mapSize-width={256}
+        shadow-mapSize-height={256}
+        shadow-camera-far={7}
+        position={[0, 2.2, 2.7]}
+      />
+
+      <fog args={['#262837', 1, 15]} />
+
+      <Suspense fallback={null}>
+        <Door />
+        <Floor />
+        <Walls />
+      </Suspense>
       <Roof />
       <Bush position={[0.8, 0.2, 2.2]} scale={[0.5, 0.5, 0.5]} />
       <Bush position={[1.4, 0.1, 2.1]} scale={[0.25, 0.25, 0.25]} />
@@ -18,21 +40,88 @@ export default function Cube() {
 }
 
 export function Floor() {
+
+  const [grassColorTexture,
+    grassAmbientOcclusionTexture,
+    grassNormalTexture,
+    grassRoughnessTexture] =
+    useTexture([
+      '/textures/grass/color.jpg',
+      '/textures/grass/ambientOcclusion.jpg',
+      '/textures/grass/normal.jpg',
+      '/textures/grass/roughness.jpg'])
+
   return (
     <mesh receiveShadow rotation={[- Math.PI * 0.5, 0, 0]} position={[0, 0, 0]} >
       <planeBufferGeometry attach="geometry" args={[50, 50]} />
-      <meshStandardMaterial attach="material" color="white" />
+      <meshStandardMaterial attach="material"
+        map={grassColorTexture}
+        aoMap={grassAmbientOcclusionTexture}
+        normalMap={grassNormalTexture}
+        roughnessMap={grassRoughnessTexture} />
     </mesh>
   );
 }
 
-export function Walls() {
-  const { y } = useControls('Walls', { y: { value: 1.25, min: 0, max: 10, step: .25 } })
+export function Door() {
+
+  const [doorColorTexture,
+    doorAlphaTexture,
+    doorAmbientOcclusionTexture,
+    doorHeightTexture,
+    doorNormalTexture,
+    doorMetalnessTexture,
+    doorRoughnessTexture] =
+    useTexture([
+      '/textures/door/color.jpg',
+      '/textures/door/alpha.jpg',
+      '/textures/door/ambientOcclusion.jpg',
+      '/textures/door/height.jpg',
+      '/textures/door/normal.jpg',
+      '/textures/door/metalness.jpg',
+      '/textures/door/roughness.jpg'])
 
   return (
-    <mesh position={[0, y, 0]}>
+    <mesh receiveShadow position={[0, 1, 2 + 0.01]} >
+      <planeBufferGeometry attach="geometry" args={[2, 2, 100, 100]} />
+      <meshStandardMaterial attach="material" transparent={true} displacementScale={0.1}
+        map={doorColorTexture}
+        alphaMap={doorAlphaTexture}
+        aoMap={doorAmbientOcclusionTexture}
+        displacementMap={doorHeightTexture}
+        normalMap={doorNormalTexture}
+        metalnessMap={doorMetalnessTexture}
+        roughnessMap={doorRoughnessTexture}
+      />
+    </mesh>
+  );
+}
+
+
+
+export function Walls() {
+  const { y } = useControls('Walls', { y: { value: 1.25, min: 0, max: 10, step: .25 } })
+  // const bricksColorTexture = useTexture('/textures/bricks/color.jpg')
+  const [bricksColorTexture,
+    bricksAmbientOcclusionTexture,
+    bricksNormalTexture,
+    bricksRoughnessTexture] =
+    useTexture([
+      '/textures/bricks/color.jpg',
+      '/textures/bricks/ambientOcclusion.jpg',
+      '/textures/bricks/normal.jpg',
+      '/textures/bricks/roughness.jpg'])
+
+  return (
+    <mesh position={[0, y, 0]} castShadow={true}>
       <boxGeometry args={[4, 2.5, 4]} />
-      <meshStandardMaterial attach="material" color="red" wireframe={true} />
+      {/* <meshStandardMaterial {...props} /> */}
+      <meshStandardMaterial attach="material"
+        map={bricksColorTexture}
+        aoMap={bricksAmbientOcclusionTexture}
+        normalMap={bricksNormalTexture}
+        roughnessMap={bricksRoughnessTexture} />
+      {/* <meshStandardMaterial attach="material" color="red" wireframe={true} /> */}
     </mesh>
   );
 }
@@ -41,7 +130,7 @@ export function Roof() {
   return (
     <mesh position={[0, 2.5 + 0.5, 0]} rotation={[0, Math.PI * 0.25, 0]}>
       <coneGeometry args={[3.5, 1, 4]} />
-      <meshStandardMaterial attach="material" color="red" wireframe={true} />
+      <meshStandardMaterial attach="material" color="#b35f45" wireframe={false} />
     </mesh>
   );
 }
@@ -52,7 +141,7 @@ export function Bush(props) {
       position={[props.position[0], props.position[1], props.position[2]]}
       scale={[props.scale[0], props.scale[1], props.scale[2]]}>
       <sphereGeometry args={[1, 16, 16]} />
-      <meshStandardMaterial attach="material" color="red" wireframe={true} />
+      <meshStandardMaterial attach="material" color="#89c854" wireframe={false} />
     </mesh>
   );
 }
@@ -66,11 +155,11 @@ class Grave extends React.Component {
       const radius = 3 + Math.random() * 6
       const x = Math.cos(angle) * radius
       const z = Math.sin(angle) * radius
-  
+
       graves.push(
-        <mesh castShadow position={[x, 0.3, z]} rotation={[0, (Math.random() - 0.5) * 0.4, (Math.random() - 0.5) * 0.4]}>
+        <mesh castShadow position={[x, 0.3, z]} rotation={[0, (Math.random() - 0.5) * 0.4, (Math.random() - 0.5) * 0.4]} key={i}>
           <boxGeometry args={[0.6, 0.8, 0.2]} />
-          <meshStandardMaterial attach="material" color="red" wireframe={true} />
+          <meshStandardMaterial attach="material" color="r#b2b6b1ed" wireframe={false} />
         </mesh>
       )
     }
