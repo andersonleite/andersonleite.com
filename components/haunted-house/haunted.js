2678a4db61a4ece16b9cd287e0428491d9824ccd
Anderson Leite <andersonlfl@gmail.com>
2021-09-02 16:42:55 -0700

    Binds point lights component to the render-loop. (useRef/useFrame)

--- components/haunted-house/haunted.js
+++ components/haunted-house/haunted.js
@@ -1,6 +1,7 @@
 import { useControls } from 'leva'
 import { useTexture } from '@react-three/drei'
-import React, { Suspense } from 'react'
+import React, { Suspense, useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
 import { Fog } from 'three'
 
 export default function Haunted() {
@@ -35,93 +36,70 @@ export default function Haunted() {
       <Bush position={[- 0.8, 0.1, 2.2]} scale={[0.4, 0.4, 0.4]} />
       <Bush position={[- 1, 0.05, 2.6]} scale={[0.15, 0.15, 0.15]} />
       <Grave />
+      <Ghosts />
     </>
   )
 }
 
 export function Floor() {
 
-  const [grassColorTexture,
-    grassAmbientOcclusionTexture,
-    grassNormalTexture,
-    grassRoughnessTexture] =
-    useTexture([
-      '/textures/grass/color.jpg',
-      '/textures/grass/ambientOcclusion.jpg',
-      '/textures/grass/normal.jpg',
-      '/textures/grass/roughness.jpg'])
+  // Load keyed textures
+  const textures = useTexture({
+    map: '/textures/grass/color.jpg',
+    aoMap: '/textures/grass/ambientOcclusion.jpg',
+    normalMap: '/textures/grass/normal.jpg',
+    roughnessMap: '/textures/grass/roughness.jpg'
+  })
 
   return (
     <mesh receiveShadow rotation={[- Math.PI * 0.5, 0, 0]} position={[0, 0, 0]} >
       <planeBufferGeometry attach="geometry" args={[50, 50]} />
-      <meshStandardMaterial attach="material"
-        map={grassColorTexture}
-        aoMap={grassAmbientOcclusionTexture}
-        normalMap={grassNormalTexture}
-        roughnessMap={grassRoughnessTexture} />
+      <meshStandardMaterial {...textures} />
     </mesh>
   );
 }
 
 export function Door() {
 
-  const [doorColorTexture,
-    doorAlphaTexture,
-    doorAmbientOcclusionTexture,
-    doorHeightTexture,
-    doorNormalTexture,
-    doorMetalnessTexture,
-    doorRoughnessTexture] =
-    useTexture([
-      '/textures/door/color.jpg',
-      '/textures/door/alpha.jpg',
-      '/textures/door/ambientOcclusion.jpg',
-      '/textures/door/height.jpg',
-      '/textures/door/normal.jpg',
-      '/textures/door/metalness.jpg',
-      '/textures/door/roughness.jpg'])
+
+
+
+  // Load keyed textures
+  const textures = useTexture({
+    map: '/textures/door/color.jpg',
+    alphaMap: '/textures/door/alpha.jpg',
+    aoMap: '/textures/door/ambientOcclusion.jpg',
+    displacementMap: '/textures/door/height.jpg',
+    normalMap: '/textures/door/normal.jpg',
+    metalnessMap: '/textures/door/metalness.jpg',
+    roughnessMap: '/textures/door/roughness.jpg'
+  })
 
   return (
     <mesh receiveShadow position={[0, 1, 2 + 0.01]} >
       <planeBufferGeometry attach="geometry" args={[2, 2, 100, 100]} />
       <meshStandardMaterial attach="material" transparent={true} displacementScale={0.1}
-        map={doorColorTexture}
-        alphaMap={doorAlphaTexture}
-        aoMap={doorAmbientOcclusionTexture}
-        displacementMap={doorHeightTexture}
-        normalMap={doorNormalTexture}
-        metalnessMap={doorMetalnessTexture}
-        roughnessMap={doorRoughnessTexture}
+        {...textures}
       />
     </mesh>
   );
 }
 
-
-
 export function Walls() {
   const { y } = useControls('Walls', { y: { value: 1.25, min: 0, max: 10, step: .25 } })
-  // const bricksColorTexture = useTexture('/textures/bricks/color.jpg')
-  const [bricksColorTexture,
-    bricksAmbientOcclusionTexture,
-    bricksNormalTexture,
-    bricksRoughnessTexture] =
-    useTexture([
-      '/textures/bricks/color.jpg',
-      '/textures/bricks/ambientOcclusion.jpg',
-      '/textures/bricks/normal.jpg',
-      '/textures/bricks/roughness.jpg'])
+
+  // Load keyed textures
+  const textures = useTexture({
+    map: '/textures/bricks/color.jpg',
+    aoMap: '/textures/bricks/ambientOcclusion.jpg',
+    normalMap: '/textures/bricks/normal.jpg',
+    roughnessMap: '/textures/bricks/roughness.jpg'
+  })
 
   return (
     <mesh position={[0, y, 0]} castShadow={true}>
       <boxGeometry args={[4, 2.5, 4]} />
-      {/* <meshStandardMaterial {...props} /> */}
-      <meshStandardMaterial attach="material"
-        map={bricksColorTexture}
-        aoMap={bricksAmbientOcclusionTexture}
-        normalMap={bricksNormalTexture}
-        roughnessMap={bricksRoughnessTexture} />
-      {/* <meshStandardMaterial attach="material" color="red" wireframe={true} /> */}
+      <meshStandardMaterial attach="material" {...textures} />
     </mesh>
   );
 }
@@ -146,24 +124,84 @@ export function Bush(props) {
   );
 }
 
-class Grave extends React.Component {
-  render() {
-    let graves = [];
+function Grave() {
+  const group = useRef()
 
-    for (let i = 0; i < 50; i++) {
-      const angle = Math.random() * Math.PI * 2
-      const radius = 3 + Math.random() * 6
-      const x = Math.cos(angle) * radius
-      const z = Math.sin(angle) * radius
+  let graves = [];
+  for (let i = 0; i < 50; i++) {
+    const angle = Math.random() * Math.PI * 2
+    const radius = 3 + Math.random() * 6
+    const x = Math.cos(angle) * radius
+    const z = Math.sin(angle) * radius
 
-      graves.push(
-        <mesh castShadow position={[x, 0.3, z]} rotation={[0, (Math.random() - 0.5) * 0.4, (Math.random() - 0.5) * 0.4]} key={i}>
-          <boxGeometry args={[0.6, 0.8, 0.2]} />
-          <meshStandardMaterial attach="material" color="r#b2b6b1ed" wireframe={false} />
-        </mesh>
-      )
-    }
-
-    return <>{graves}</>;
+    graves.push(
+      <mesh castShadow position={[x, 0.3, z]} rotation={[0, (Math.random() - 0.5) * 0.4, (Math.random() - 0.5) * 0.4]} key={i}>
+        <boxGeometry args={[0.6, 0.8, 0.2]} />
+        <meshStandardMaterial attach="material" color="r#b2b6b1ed" wireframe={false} />
+      </mesh>
+    )
   }
+
+  return (
+    <group ref={group} dispose={null}>
+      {graves}
+    </group>
+  )
+}
+
+
+function Ghosts() {
+  const ghost1 = useRef()
+  const ghost2 = useRef()
+  const ghost3 = useRef()
+
+
+  useFrame((state, delta) => {
+    // This function runs 60 times/second, it binds this component to the render-loop.
+    // On unmount this subscription is cleaned up automatically.
+
+    const elapsedTime = state.clock.getElapsedTime()
+
+    // Ghosts
+    const ghost1Angle = elapsedTime * 0.5
+    ghost1.current.position.x = Math.cos(ghost1Angle) * 4
+    ghost1.current.position.z = Math.sin(ghost1Angle) * 4
+    ghost1.current.position.y = Math.sin(elapsedTime * 3)
+
+    const ghost2Angle = - elapsedTime * 0.32
+    ghost2.current.position.x = Math.cos(ghost2Angle) * 5
+    ghost2.current.position.z = Math.sin(ghost2Angle) * 5
+    ghost2.current.position.y = Math.sin(elapsedTime * 4) + Math.sin(elapsedTime * 2.5)
+
+    const ghost3Angle = - elapsedTime * 0.18
+    ghost3.current.position.x = Math.cos(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.32))
+    ghost3.current.position.z = Math.sin(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.5))
+    ghost3.current.position.y = Math.sin(elapsedTime * 4) + Math.sin(elapsedTime * 2.5)
+  })
+
+  return (
+    <group dispose={null}>
+      <pointLight ref={ghost1}
+        args={['#fff', 2, 3]}
+        castShadow
+        shadow-mapSize-width={256}
+        shadow-mapSize-height={256}
+        shadow-camera-far={7}
+      />
+      <pointLight ref={ghost2}
+        args={['#fff', 2, 3]}
+        castShadow
+        shadow-mapSize-width={256}
+        shadow-mapSize-height={256}
+        shadow-camera-far={7}
+      />
+      <pointLight ref={ghost3}
+        args={['#fff', 2, 3]}
+        castShadow
+        shadow-mapSize-width={256}
+        shadow-mapSize-height={256}
+        shadow-camera-far={7}
+      />
+    </group>
+  )
 }
