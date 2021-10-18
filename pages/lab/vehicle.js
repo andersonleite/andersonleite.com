6d38d422c458fe578d81ac0a25f6904e0ca726c2
“Anderson <Andersonlfl@gmail.com>
2021-10-19 08:18:31 +0900

    Vehicle, pt2

--- pages/lab/vehicle.js
+++ pages/lab/vehicle.js
@@ -5,6 +5,8 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Environment } from '@react-three/drei'
 import { Physics, useCylinder, usePlane } from '@react-three/cannon'
 
+import Vehicle from '../../components/vehicle/vehicle'
+
 export default function VehiclePage() {
   return (
     <div className={styles.full}>
@@ -13,19 +15,13 @@ export default function VehiclePage() {
         <color attach="background" args={['#171720']} />
         <ambientLight intensity={0.1} />
         <spotLight position={[10, 10, 10]} angle={0.5} intensity={1} castShadow penumbra={1} />
-
         <Physics broadphase="SAP" contactEquationRelaxation={4} friction={1e-3} allowSleep>
-
-          {/* TODO  */}
-          {/* Part II */}
-          {/* https://sketchfab.com/3d-models/mazda-rx7-stylised-898e9be8c8964f6298188a2901216e80 */}
-
+          <Vehicle position={[0, 2, 0]} rotation={[0, -Math.PI / 4, 0]} angularVelocity={[0, 0.5, 0]} wheelRadius={0.3} />
           <Plane rotation={[-Math.PI / 2, 0, 0]} userData={{ id: 'floor' }} />
           <Pillar position={[-5, 2.5, -5]} userData={{ id: 'pillar-1' }} />
           <Pillar position={[0, 2.5, -5]} userData={{ id: 'pillar-2' }} />
           <Pillar position={[5, 2.5, -5]} userData={{ id: 'pillar-3' }} />
         </Physics>
-
         <Suspense fallback={null}>
           <Environment preset="night" />
         </Suspense>
