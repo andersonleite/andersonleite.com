cc0c132c08e832c4421366e417c0d83975d55109
Anderson Leite <andersonlfl@gmail.com>
2022-04-17 16:51:45 +0900

    mnp

--- components/Scene.js
+++ components/Scene.js
@@ -1,22 +1,20 @@
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from '@react-three/drei'
-import { useControls } from "leva"
+
+import * as THREE from 'three';
 
 const Scene =(props) =>{
 
-  const { visible } = useControls('Axes Helper', { visible: true })
-  const { size } = useControls('Axes Helper', { size: { value: 1, min: 0, max: 100, step: 1 } })
+  const OrthographicCamera = new THREE.OrthographicCamera(-1, 1, 1, -1, -10, 10)
 
   return (
 
 
     <Canvas
       pixelRatio={window.devicePixelRatio}
-      camera={{ position: [props.x || 0, props.y || 0, props.z || 3] }
-      }>
-      <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
+      camera={OrthographicCamera}>
+
       {props.children}
-      <axesHelper args={[size]} visible={visible} />
+      
     </Canvas >
   )
 }
---
+++ components/monopo/TCanvas.tsx
@@ -0,0 +1,23 @@
+import React, { Suspense, VFC } from 'react';
+import * as THREE from 'three';
+import { Canvas } from '@react-three/fiber';
+import {Background} from '../../components/monopo/Background'
+import { Lense } from '../../components/monopo/Lense';
+
+// import {
+// 	enFragmentShader, enVertexShader, jpFragmentShader, jpVertexShader
+// } from '../../modules/glsl/shader';
+// import { Background } from './Background';
+// import { Lense } from './Lense';
+// import { TextPlane } from './TextPlane';
+
+export const TCanvas: VFC = () => {
+	const OrthographicCamera = new THREE.OrthographicCamera(-1, 1, 1, -1, -10, 10)
+
+	return (
+    <div>
+
+
+    </div>
+	)
+}
